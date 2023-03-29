6f09ea0fd9d50af31bf899a80c96705b12ad5e8c
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-03-28 23:05:04 -0300

    Cambios identacion

--- src/dominio/Principal.java
+++ src/dominio/Principal.java
@@ -10,20 +10,20 @@ import clases.EntradaDeportiva;
 public class Principal {
 
 	public static void main(String[] args) {
-		//Creacion de los deportes y sus precios
+		// Creacion de los deportes y sus precios
 		Deporte futbolNacional = new Deporte("Futbol", 300, false);
 		Deporte hockeyInternacional = new Deporte("Hockey", 380, true);
-		
-		//Creacion de entradas deportivas y calculo de precio
-		Entrada entradaDeportiva = new EntradaDeportiva("Super clasico", LocalDate.of(2023, 3, 28), LocalTime.of(18, 30),
-				200, 0.0, futbolNacional);
+
+		// Creacion de entradas deportivas y calculo de precio
+		Entrada entradaDeportiva = new EntradaDeportiva("Super clasico", LocalDate.of(2023, 3, 28),
+				LocalTime.of(18, 30), 200, 0.0, futbolNacional);
 		entradaDeportiva.calcularPrecio();
-		
-		Entrada entradaDeportiva2 = new EntradaDeportiva("Hockey internacional!", LocalDate.of(2023, 3, 28), LocalTime.of(18, 30),
-				200, 0.0, hockeyInternacional);
+
+		Entrada entradaDeportiva2 = new EntradaDeportiva("Hockey internacional!", LocalDate.of(2023, 3, 28),
+				LocalTime.of(18, 30), 200, 0.0, hockeyInternacional);
 		entradaDeportiva2.calcularPrecio();
-		
-		//Mostrar por consola resutlados
+
+		// Mostrar por consola resutlados
 		System.out.println(entradaDeportiva.toString());
 		System.out.println(entradaDeportiva2.toString());
 	}
